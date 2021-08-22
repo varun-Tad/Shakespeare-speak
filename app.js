a38445df83e170f9700989fc643a2de941f8f392
varun-Tad <varuntad141@gmail.com>
2021-08-22 23:22:45 +0530

    add button feature

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
+
+
+function clickHandler(){
+    outputDiv.innerText = "bwibebviebvieb";
+}
+
+
+btnTranslate.addEventListener('click',clickHandler);
